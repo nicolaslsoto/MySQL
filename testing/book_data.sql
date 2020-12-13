c8012920ca709c9c11dcb6dbbb027e0a29842120
Nicolas Soto <nsoto0216@gmail.com>
2020-12-13 14:53:50 -0500

    updated data query

--- testing/book_data.sql
+++ testing/book_data.sql
@@ -1,14 +1,14 @@
 CREATE TABLE books 
-	(
-		book_id INT NOT NULL AUTO_INCREMENT,
-		title VARCHAR(100),
-		author_fname VARCHAR(100),
-		author_lname VARCHAR(100),
-		released_year INT,
-		stock_quantity INT,
-		pages INT,
-		PRIMARY KEY(book_id)
-	);
+    (
+        book_id INT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(100),
+        author_fname VARCHAR(100),
+        author_lname VARCHAR(100),
+        released_year INT,
+        stock_quantity INT,
+        pages INT,
+        PRIMARY KEY(book_id)
+    );
 
 INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
 VALUES
