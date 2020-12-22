81c906911f08ea0c57a4f40bf7677c9adefc9716
nsoto0216@gmail.com <nsoto0216@gmail.com>
2020-12-21 23:40:44 -0500

    added repo inside mysql_node

---
+++ mysqlpy.py
@@ -0,0 +1,10 @@
+# import mysql.connector
+# conn = mysql.connector.connect(option_files = 'my.conf', option_groups = ['connection_details'])
+import MySQLdb
+conn = MySQLdb.connect(host="127.0.0.1",port=3306,user="root",passwd="305g305g",db="ig_clone")
+c = conn.cursor()
+q = "SELECT * FROM tags;"
+c.execute(q)
+for row in c.fetchall():
+    print(row)
+conn.close()
